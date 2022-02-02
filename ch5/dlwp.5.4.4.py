d9bde00f2877907c2a2d0c69417b6b8b1cf8fffa
Bogart <roygunter@hushmail.com>
2022-02-01 20:57:11 -0500

    dlwp.5.4.4

--- ch5/dlwp.5.4.4.py
+++ ch5/dlwp.5.4.4.py
@@ -63,6 +63,55 @@ history_larger_model = model.fit(
         batch_size=512,
         validation_split=0.4)
 
+# Listing 5.13 Adding L2 weight regularization to the model
+from tensorflow.keras import regularizers
+model = keras.Sequential([
+    layers.Dense(16, kernel_regularizer=regularizers.l2(0.002), activation="relu"),
+    layers.Dense(16, kernel_regularizer=regularizers.l2(0.002), activation="relu"),
+    layers.Dense(1, activation="sigmoid")
+])
+model.compile(optimizer="rmsprop",
+        loss="binary_crossentropy",
+        metrics=["accuracy"])
+history_l2_reg = model.fit(
+        train_data,
+        train_labels,
+        epochs=20,
+        batch_size=512,
+        validation_split=0.4)
+
+# L1
+model = keras.Sequential([
+    layers.Dense(16, kernel_regularizer=regularizers.l1(0.001), activation="relu"),
+    layers.Dense(16, kernel_regularizer=regularizers.l1(0.001), activation="relu"),
+    layers.Dense(1, activation="sigmoid")
+])
+model.compile(optimizer="rmsprop",
+        loss="binary_crossentropy",
+        metrics=["accuracy"])
+history_l1_reg = model.fit(
+        train_data,
+        train_labels,
+        epochs=20,
+        batch_size=512,
+        validation_split=0.4)
+
+# L2
+model = keras.Sequential([
+    layers.Dense(16, kernel_regularizer=regularizers.l1_l2(l1=0.001, l2=0.001), activation="relu"),
+    layers.Dense(16, kernel_regularizer=regularizers.l1_l2(l1=0.001, l2=0.001), activation="relu"),
+    layers.Dense(1, activation="sigmoid")
+])
+model.compile(optimizer="rmsprop",
+        loss="binary_crossentropy",
+        metrics=["accuracy"])
+history_l1_l2_reg = model.fit(
+        train_data,
+        train_labels,
+        epochs=20,
+        batch_size=512,
+        validation_split=0.4)
+
 # Figure 5.17/5.18
 import matplotlib.pyplot as plt
 
@@ -79,7 +128,19 @@ plt.plot(epochs, val_loss, "b-", label="val loss smaller")
 val_loss = history_larger_model.history["val_loss"]
 plt.plot(epochs, val_loss, "r-", label="val loss larger")
 
-plt.title("Original model vs. smaller model on IMDB review classification")
+# history_l2_reg
+val_loss = history_l2_reg.history["val_loss"]
+plt.plot(epochs, val_loss, "r--", label="val loss l2 reg")
+
+# history_l1_reg
+val_loss = history_l1_reg.history["val_loss"]
+plt.plot(epochs, val_loss, "g-", label="val loss l1 reg")
+
+# history_l1_l2_reg
+val_loss = history_l1_l2_reg.history["val_loss"]
+plt.plot(epochs, val_loss, "g--", label="val loss l1_l2 reg")
+
+plt.title("Original vs. smaller vs. larger vs. l2 reg on IMDB review classification")
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.legend()
