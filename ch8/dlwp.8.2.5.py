6c23d3f7a63a6b0d97e3faa0331bb92f501559d7
R Gunter <roygunter@hushmail.com>
2022-02-25 22:03:57 -0500

    dlwp 8.2.5

--- ch8/dlwp.8.2.5.py
+++ ch8/dlwp.8.2.5.py
@@ -49,6 +49,67 @@ for images, _ in train_dataset.take(1):
         # Display the first image in the output batch.
         # For each of the nine iterations, this is a different augmentation of the same image.
         plt.imshow(augmented_images[0].numpy().astype("uint8"))
-        
+plt.figure()        
+#plt.show()
+
+# Listing 8.16 Defining a new convnet that includes image augmentation and dropout
+inputs = keras.Input(shape=(180, 180, 3))
+x = data_augmentation(inputs)
+x = layers.Rescaling(1./255)(inputs)
+x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=5256, kernel_size=3, activation="relu")(x)
+x = layers.Flatten()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.summary()
+
+model.compile(loss="binary_crossentropy",
+            optimizer="rmsprop",
+            metrics=["accuracy"])
+
+#
+# Listing 8.17 Training the regularized convnet
+callbacks = [
+        keras.callbacks.ModelCheckpoint(
+            filepath="convnet_from_scratch_with_augmentation.keras",
+            save_best_only=True,
+            metrics="val_loss")
+]
+history = model.fit(
+        train_dataset,
+        epochs=100,
+        validation_data=validation_dataset,
+        callbacks=callbacks)
+
+#
+# Figure 8.11 Training and validation metrics with data augmentation
+import matplotlib.pyplot as plt
+accuracy = history.history["accuracy"]
+val_accuracy = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+epochs = range(1, len(accuracy) + 1)
+plt.plot(epochs, accuracy, "bo", label="Train Acc")
+plt.plot(epochs, val_accuracy, "b", label="Val Acc")
+plt.title("Training and validation accuracy/loss")
+plt.plot(epochs, loss, "ro", label="Train Loss")
+plt.plot(epochs, val_loss, "r", label="Val Loss")
+plt.legend()
 plt.show()
+
+#
+# Listing 8.18 Evaluating the model on the test set
+print("Listing 8.18 Evaluating the model on the test set.")
+test_model = keras.models.load_model("convnet_from_scratch_with_augmentation.keras")
+test_loss, test_acc = test_model.evaluate(test_dataset)
+print(f"Test accuracy: {test_acc:.3f}")
+
 
