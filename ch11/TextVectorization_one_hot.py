2a06ce039c9f4fec416fae5b350527205453effa
R Gunter <roygunter@hushmail.com>
2022-04-08 21:59:33 -0400

    dlwp 11.3.3 embedding

---
+++ ch11/TextVectorization_one_hot.py
@@ -0,0 +1,95 @@
+#
+# https://towardsdatascience.com/building-a-one-hot-encoding-layer-with-tensorflow-f907d686bf39
+
+# The experimental TextVectorization layer can be used to standardize 
+# and tokenize sequences of strings, such as sentences, but for our use case, 
+# we’ll simply convert individual string categories into integer indices.
+import pandas as pd
+from tensorflow.keras import layers
+
+colors_df = pd.DataFrame(data=[['red'],['blue'],['green'],['blue']], columns=['color'])
+
+text_vectorization = layers.experimental.preprocessing.TextVectorization(output_sequence_length=1)
+text_vectorization.adapt(colors_df.values)
+
+print('Red index:', text_vectorization.call([['red']]))
+print('Blue index:', text_vectorization.call([['blue']]))
+print('Green index:', text_vectorization.call([['green']]))
+
+print(text_vectorization.get_vocabulary()) # prints [b'blue', b'red', b'green']
+
+# The OneHotEncodingLayer Class
+# Finally, we can now create the Class that will represent a One Hot Encoding Layer in a neural network.
+class OneHotEncodingLayer(layers.experimental.preprocessing.PreprocessingLayer):
+  def __init__(self, vocabulary=None, depth=None, minimum=None):
+    super().__init__()
+    self.vectorization = layers.experimental.preprocessing.TextVectorization(output_sequence_length=1)  
+
+    if vocabulary:
+      self.vectorization.set_vocabulary(vocabulary)
+    self.depth = depth   
+    self.minimum = minimum
+
+  def adapt(self, data):
+    self.vectorization.adapt(data)
+    vocab = self.vectorization.get_vocabulary()
+    self.depth = len(vocab)
+    indices = [i[0] for i in self.vectorization([[v] for v in vocab]).numpy()]
+    self.minimum = min(indices)
+
+  def call(self,inputs):
+    vectorized = self.vectorization.call(inputs)
+    subtracted = tf.subtract(vectorized, tf.constant([self.minimum], dtype=tf.int64))
+    encoded = tf.one_hot(subtracted, self.depth)
+    return layers.Reshape((self.depth,))(encoded)
+
+  def get_config(self):
+    return {'vocabulary': self.vectorization.get_vocabulary(), 'depth': self.depth, 'minimum': self.minimum}
+
+# Using the Custom Layer
+# Now we can try the new layer out in a simple Neural Network.
+import pandas as pd
+from tensorflow.keras import layers, models
+
+colors_df = pd.DataFrame(data=[[5,'yellow'],[1,'red'],[2,'blue'],[3,'green'],[4,'blue'],[7,'purple']], columns=['id', 'color'])
+
+categorical_input = layers.Input(shape=(1,), dtype=tf.string)
+one_hot_layer = OneHotEncodingLayer()
+one_hot_layer.adapt(colors_df['color'].values)
+encoded = one_hot_layer(categorical_input)
+
+numeric_input = layers.Input(shape=(1,), dtype=tf.float32)
+
+concat = layers.concatenate([numeric_input, encoded])
+
+model = models.Model(inputs=[numeric_input, categorical_input], outputs=[concat])
+predicted = model.predict([colors_df['id'], colors_df['color']])
+print(predicted)
+# [[5. 0. 1. 0. 0. 0.]
+#  [1. 0. 0. 1. 0. 0.]
+#  [2. 1. 0. 0. 0. 0.]
+#  [3. 0. 0. 0. 0. 1.]
+#  [4. 1. 0. 0. 0. 0.]
+#  [7. 0. 0. 0. 1. 0.]]
+
+#
+# Notice that it One Hot Encodes the color category in the same way as before, 
+# so we know that the subsequent layers of our model will be provided the same 
+# features in the same order as they appeared during training.
+config = model.get_config()
+with tf.keras.utils.custom_object_scope({'OneHotEncodingLayer': OneHotEncodingLayer}):
+  loaded_model = tf.keras.Model.from_config(config)
+
+predicted = new_model.predict([colors_df['id'], colors_df['color']])
+print(predicted)
+# [[5. 0. 1. 0. 0. 0.]
+#  [1. 0. 0. 1. 0. 0.]
+#  [2. 1. 0. 0. 0. 0.]
+#  [3. 0. 0. 0. 0. 1.]
+#  [4. 1. 0. 0. 0. 0.]
+#  [7. 0. 0. 0. 1. 0.]]
+
+#
+# You can find a notebook containing all of the code examples here: 
+# https://github.com/gnovack/tf-one-hot-encoder/blob/master/OneHotEncoderLayer.ipynb
+
---
+++ ch11/dlwp.11.3.3-embedding.py
@@ -0,0 +1,70 @@
+# Suppress warnings
+import os, pathlib
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Force CPU use for keras.
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+print("11.3.3 Processing words as a sequence: The sequencemodel approach")
+import tensorflow as tf
+from tensorflow import keras
+batch_size = 16
+
+train_ds = keras.utils.text_dataset_from_directory(
+                "/root/src/aclImdb/train/", batch_size=batch_size)
+
+val_ds = keras.utils.text_dataset_from_directory(
+                "/root/src/aclImdb/val/", batch_size=batch_size)
+
+test_ds = keras.utils.text_dataset_from_directory(
+                "/root/src/aclImdb/test/", batch_size=batch_size)
+
+text_only_train_ds = train_ds.map(lambda x, y: x)
+
+print("Listing 11.12 Preparing integer sequence datasets")
+from tensorflow.keras import layers
+
+max_length = 600
+max_tokens = 20000
+text_vectorization = layers.TextVectorization(
+        max_tokens=max_tokens,
+        output_mode="int",
+        # In order to keep a manageable input size, we'll truncate the inputs after the first 600 words.
+        output_sequence_length=max_length,
+)
+text_vectorization.adapt(text_only_train_ds)
+
+int_train_ds = train_ds.map(
+                lambda x, y: (text_vectorization(x), y),
+                num_parallel_calls=tf.data.AUTOTUNE)
+int_val_ds = val_ds.map(
+                lambda x, y: (text_vectorization(x), y),
+                num_parallel_calls=tf.data.AUTOTUNE)
+int_test_ds = test_ds.map(
+                lambda x, y: (text_vectorization(x), y),
+                num_parallel_calls=tf.data.AUTOTUNE)
+
+print("11.16 Model that uses an Embedding layer trained from scratch")
+inputs = keras.Input(shape=(None,), dtype="int64")
+embedded = layers.Embedding(input_dim=max_tokens, output_dim=256)(inputs)
+x = layers.Bidirectional(layers.LSTM(32))(embedded)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs, outputs)
+model.compile(optimizer="rmsprop",
+        loss="binary_crossentropy",
+        metrics=["accuracy"])
+model.summary()
+
+callbacks = [
+        keras.callbacks.ModelCheckpoint("one_hot_bidir_gru.keras",
+            save_best_only=True)
+]
+model.fit(int_train_ds,
+        validation_data=int_val_ds,
+        epochs=10,
+        callbacks=callbacks)
+model = keras.models.load_model("one_hot_bidir_gru.keras")
+print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
+
