583f6a867b5404df11edc03142f78a8070e273bd
R Gunter <roygunter@hushmail.com>
2022-04-10 18:26:07 -0400

    dlwp 11.3.4

---
+++ ch11/dlwp.11.3.4.py
@@ -0,0 +1,107 @@
+# Suppress warnings
+import os, pathlib
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Force CPU use for keras.
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+print("11.3.4 Using pretrained word embeddings")
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+batch_size = 16
+
+train_ds = keras.utils.text_dataset_from_directory(
+                "/root/src/aclImdb/train/", batch_size=batch_size)
+
+val_ds = keras.utils.text_dataset_from_directory(
+                "/root/src/aclImdb/val/", batch_size=batch_size)
+
+test_ds = keras.utils.text_dataset_from_directory(
+                "/root/src/aclImdb/test/", batch_size=batch_size)
+
+max_length = 600
+max_tokens = 20000
+text_vectorization = layers.TextVectorization(
+        max_tokens=max_tokens,
+        output_mode="int",
+        output_sequence_length=max_length,
+)
+
+text_only_train_ds = train_ds.map(lambda x, y: x)
+
+int_train_ds = train_ds.map(
+                lambda x, y: (text_vectorization(x), y),
+                num_parallel_calls=tf.data.AUTOTUNE)
+int_val_ds = val_ds.map(
+                lambda x, y: (text_vectorization(x), y),
+                num_parallel_calls=tf.data.AUTOTUNE)
+int_test_ds = test_ds.map(
+                lambda x, y: (text_vectorization(x), y),
+                num_parallel_calls=tf.data.AUTOTUNE)
+
+print("Listing 11.18 Parsing the GloVe word-embeddings file")
+import numpy as np
+path_to_glove_file = "/root/src/glove.6B/glove.6B.100d.txt"
+
+embeddings_index = {}
+with open(path_to_glove_file) as f:
+    for line in f:
+        word, coefs = line.split(maxsplit=1)
+        coefs = np.fromstring(coefs, "f", sep=" ")
+        embeddings_index[word] = coefs
+
+print(f"Found {len(embeddings_index)} word vectors.")
+
+print("Listing 11.19 Preparing the GloVe word-embeddings matrix")
+embedding_dim = 100
+
+# Retrieve the vocabulary indexed by our previous TextVectorization layer.
+vocabulary = text_vectorization.get_vocabulary()
+
+# Use it to create a mapping from words to their index in the vocabulary.
+word_index = dict(zip(vocabulary, range(len(vocabulary))))
+
+# Prepare a matrix that we'll fill with the GloVe vectors.
+embedding_matrix = np.zeros((max_tokens, embedding_dim))
+for word, i in word_index.items():
+    if i < max_tokens:
+        embedding_vector = embeddings_index.get(word)
+    # Fill emtry i in the matrix with the word vector for index i.
+    # Words not foundin the embedding index will be all zeros.
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+embedding_layer = layers.Embedding(
+        max_tokens,
+        embedding_dim,
+        embeddings_initializer=keras.initializers.Constant(embedding_matrix),
+        trainable=False,
+        mask_zero=True
+)
+
+print("Listing 11.20 Model that uses a pretrained Embedding layer")
+inputs = keras.Input(shape=(None,), dtype="int64")
+embedded = embedding_layer(inputs)
+x = layers.Bidirectional(layers.LSTM(32))(embedded)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs, outputs)
+model.compile(optimizer="rmsprop",
+        loss="binary_crossentropy",
+        metrics=["accuracy"])
+model.summary()
+
+callbacks = [
+        keras.callbacks.ModelCheckpoint("glove_embeddings_sequence_model.keras",
+            save_best_only=True)
+]
+model.fit(int_train_ds,
+        validation_data=int_val_ds,
+        epochs=10,
+        callbacks=callbacks)
+model=keras.load_model("glove_embeddings_sequence_model.keras")
+print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
+
