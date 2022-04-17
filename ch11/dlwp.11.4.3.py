f8c7bc7521c4f4b8c1be10b22d3107a51efab95a
R Gunter <roygunter@hushmail.com>
2022-04-16 20:43:13 -0400

    dlwp 11.4.3

--- ch11/dlwp.11.4.3.py
+++ ch11/dlwp.11.4.3.py
@@ -66,8 +66,8 @@ class TransformerEncoder(layers.Layer):
                 [layers.Dense(dense_dim, activation="relu"),
                  layers.Dense(embed_dim),]
         )
-        self.layernorn_1 = layers.LayerNormalization()
-        self.layernorn_2 = layers.LayerNormalization()
+        self.layernorm_1 = layers.LayerNormalization()
+        self.layernorm_2 = layers.LayerNormalization()
 
     # Computation goes in call().
     def call(self, inputs, mask=None):
@@ -91,7 +91,42 @@ class TransformerEncoder(layers.Layer):
             "num_heads": self.num_heads,
             "dense_dim": self.dense_dim,
         })
+        return config
 
+print("Listing 11.22 Using the Transformer encoder for text classification")
+vocab_size = 20000
+embed_dim = 256
+num_heads = 2
+dense_dim = 32
+
+inputs = keras.Input(shape=(None,), dtype="int64")
+x = layers.Embedding(vocab_size, embed_dim)(inputs)
+x = TransformerEncoder(embed_dim, dense_dim, num_heads)(x)
+# Since TransformerEncoder returns full sequences, we need to reduce each
+# sequence to a single vector for classification via a global pooling layer.
+x = layers.GlobalMaxPooling1D()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs, outputs)
+model.compile(optimizer="rmsprop",
+        loss="binary_crossentropy",
+        metrics=["accuracy"])
+model.summary()
+
+print("Listing 11.23 Training and evaluating the Transformer encoder based model")
+callbacks = [
+        keras.callbacks.ModelCheckpoint("transformer_encoder.keras",
+        save_best_only=True)
+]
+model.fit(int_train_ds,
+        validation_data=int_val_ds,
+        epochs=20,
+        callbacks=callbacks)
+model = keras.models.load_model(
+        "transformer_encoder.keras",
+        # Provide the custom TransformerEncoder class to the model-loading process
+        custom_objects={"TransformerEncoder": TransformerEncoder})
+print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
 
 
 
