9dafd218f4e925e374732ebd9fd5d542d7a7550d
R Gunter <roygunter@hushmail.com>
2022-06-30 17:32:01 -0400

    dlwp 12.1.4

--- ch12/dlwp.12.1.4.py
+++ ch12/dlwp.12.1.4.py
@@ -20,5 +20,21 @@ dataset = dataset.map(lambda x: tf.strings.regex_replace(x, "<br />", " "))
 print("list(dataset)[0]:")
 print(list(dataset)[0])
 
+print("")
 print("Listing 12.4 Preparing a TextVectorization layer")
+from tensorflow.keras.layers import TextVectorization
+
+sequence_length = 100
+# We'll only consider the 15,000 most common words--
+# anything else will be treated as out-of-vocabulary token, "[UNK]".
+vocab_size = 15000
+text_vectorization = TextVectorization(
+        max_tokens=vocab_size,
+        # We want to return integer word index sequences.
+        output_mode="int",
+        # We'll work with inputs and targets of length 100
+        # (but since we'll offset the targets by 1, the model will actually see sequences of length 99).
+        output_sequence_length=sequence_length,
+)
+text_vectorization.adapt(dataset)
 
