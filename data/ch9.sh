0073919be17459a8be3038b38af9e6100b4d56e9
R Gunter <roygunter@hushmail.com>
2022-10-07 16:59:12 -0400

    dlwp: Scripts to download data ch9

--- data/ch9.sh
+++ data/ch9.sh
@@ -2,4 +2,15 @@
 # input_dir = "/root/src/data/images/"
 # target_dir = "/root/src/data/annotations/trimaps/"
 
+# WGET the data
 wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz -P /tmp/
+wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz -P /tmp/
+
+# Extract to data directory
+tar xvf /tmp/images.tar.gz -C /root/src/data/
+tar xvf /tmp/annotations.tar.gz -C /root/src/data/
+
+# Delete the archives
+rm -rf /tmp/images.tar.gz*
+rm -rf /tmp/annotations.tar.gz*
+
