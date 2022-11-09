f013e06e6cf76422f7e488cb9497ab579ba6b460
R Gunter <roygunter@hushmail.com>
2022-11-09 17:03:58 -0500

    dlwp.12.4.4.py WIP

---
+++ ch12/dlwp.12.4.4.py
@@ -0,0 +1,24 @@
+# Suppress warnings
+import os, pathlib
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Force CPU use for keras.
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+print("12.4.4 Implementing a VAE with Keras")
+
+print("Listing 12.24 VAE encoder network")
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Dimensionality of the latent space: a 2D plane.
+latent_dim = 2
+
+encoder_inputs = keras.Input(shape=(28, 28, 1))
+x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+x = layers.Flatten()(x)
+x = layers.Dense(16, activation="relu")(x)
+
+
