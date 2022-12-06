a00634cc2ee5dff5d5067ac56c8052dabbd3854d
R Gunter <roygunter@hushmail.com>
2022-12-06 13:02:16 -0500

    WIP dlwp.12.5.3.py

--- ch12/dlwp.12.5.3.py
+++ ch12/dlwp.12.5.3.py
@@ -79,5 +79,80 @@ generator = keras.Sequential(
 
 generator.summary()
 
+print("Listing 12.36 The GAN Model")
+import tensorflow as tf
+class GAN(keras.Model):
+    def __init__(self, discriminator, generator, latent_dim):
+        super().__init__()
+        self.discriminator = discriminator
+        self.generator = generator
+        self.latent_dim = latent_dim
+        # Set up metrics to track the two losses over each training epoch
+        self.d_loss_metric = keras.metrics.Mean(name="d_loss")
+        self.g_loss_metric = keras.metrics.Mean(name="g_loss")
+
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
+        super(GAN, self).compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.loss_fn = loss_fn
+
+    @property
+    def metrics(self):
+        return [self.d_loss_metric, self.g_loss_metric]
+
+    def train_step(self, real_images):
+        # Sample random points in the latest space
+        batch_size = tf.shape(real_images)[0]
+        random_latent_vectors = tf.random.normal(
+                shape=(batch_size, self.latent_dim)
+                )
+        # Decodes them to fake images
+        generated_images = self.generator(random_latent_vectors)
+        # Combines them with real images
+        combined_images = tf.concat([generated_images, real_images], axis=0)
+        # Assembles labels, discriminating real from fake images
+        labels = tf.concat(
+                [tf.ones((batch_size, 1)), tf-zeros((batch_size, 1))],
+                axis=0
+                )
+        # Adds random noise to the labels--an importabnt trick
+
+        # Trains the discriminator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(combined_images)
+            d_loss = self.loss_fn(labels, predictions)
+        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+        self.d_optimizer.apply_gradients(
+                zip(grads, self.discriminator.trainable_weights)
+                )
+
+        # Samples random points in the latent space
+        random_latent_vectors = tf.random.normal(
+                shape=(batch_size, self.latent_dim))
+
+        # Assembles labels that say "these are all real images" (it's a lie!)
+        misleading_labels = tf.zeros((batch_size, 1))
+
+        # Trains the generator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(
+                    self.generator(random_latent_vectors))
+            g_loss = self.loss_fn(misleading_labels, predictions)
+        grads = tape.gradient(g_loss, self.generator.trainable_weights)
+        self.g_optimizer.apply_gradients(
+                zip(grads, self.generator.trainable_weights))
+
+        self.d_loss_metric.update_state(d_loss)
+        self.g_loss_metric.update_state(g_loss)
+        return {"d_loss": self.d_loss_metric.result(),
+                "g_loss": self.g_loss_metric.result()}
+
+print("Listing 12.37 A callback that samples generated images during training") 
+
+
+
+
+
 
 
